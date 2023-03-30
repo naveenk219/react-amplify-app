6091238eb95d6500145ec942146cf8f395c4ba1c
Naveen <knaveen@velankanigroup.com>
2023-03-30 15:58:57 +0530

    updated headline

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
-        <h1>Hello Wecome to AWS Amplify</h1>
+        <h1>Hello Wecome to AWS Amplify TEST</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
