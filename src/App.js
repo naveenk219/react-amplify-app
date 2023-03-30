e15c2e883f018e7c5363fa6938b51a3700270dfc
Naveen <knaveen@velankanigroup.com>
2023-03-30 15:44:05 +0530

    updated headline

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        <h1>Hello Wecome to AWS Amplify</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
